8102a168896b77b5cc1e83dc504e36e402683f84
CentroneF <centrone.francesco@gmail.com>
2015-06-21 19:01:46 +0200

    Connessione a redis funzionante

--- src/main/java/it/shortener/RedisConnection.java
+++ src/main/java/it/shortener/RedisConnection.java
@@ -4,10 +4,13 @@ import redis.clients.jedis.Jedis;
 
 public class RedisConnection {
 	   public static void main(String[]args) throws Exception {
-	      Jedis jedis = new Jedis("172.17.0.8",6379);
+	      Jedis jedis = new Jedis("127.0.0.1",6379);
 	      jedis.connect();
 	      System.out.println("Connection to server sucessfully");
 	      System.out.println("Server is running: "+jedis.ping());
+	      jedis.set("tutorial-name", "Redis tutorial");
+	      // Get the stored data and print it
+	      System.out.println("Stored string in redis:: "+ jedis.get("tutorial-name"));
 	      jedis.close();
 	 }
 }
